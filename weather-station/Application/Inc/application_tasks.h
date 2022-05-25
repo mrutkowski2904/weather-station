9f0eadd9e3cd7e8ccda9b119418bb823a58fd60c
Maciej Rutkowski <303179@stud.umk.pl>
2022-05-25 15:32:21 +0200

    Utworzenie projektu

---
+++ weather-station/Application/Inc/application_tasks.h
@@ -0,0 +1,6 @@
+#ifndef INC_APPLICATION_TASKS_H_
+#define INC_APPLICATION_TASKS_H_
+
+void StartDisplayTask(void *argument);
+
+#endif /* INC_APPLICATION_TASKS_H_ */
---
+++ weather-station/Application/Src/application_tasks.c
@@ -0,0 +1,7 @@
+#include "application_tasks.h"
+
+void StartDisplayTask(void *argument) {
+	for (;;) {
+
+	}
+}
